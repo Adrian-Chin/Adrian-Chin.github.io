bedef377c82f01ee767cf24037189cebfc1feff4
Adrian-Chin <adrianc159@nycstudents.net>
2024-06-07 01:24:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const h1Element = document.querySelector('h1');
-  h1Element.addEventListener('mouseover', function() {
-
-  });
-  h1Element.addEventListener('mouseout', function() {
-    h1Element.style.textDecoration = 'none';
-    h1Element.style.fontWeight = 'normal';
+  const aboutLink = document.querySelector('nav ul li a[href="#about"]');
+  aboutLink.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default link behavior
+    window.location.href = 'about.html'; // Redirect to about.html
   });
 });
